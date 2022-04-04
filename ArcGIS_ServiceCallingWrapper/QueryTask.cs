2d72e7634cdb05592202ae911460b4d2753df436
Shehab.github <shehab2007@gmail.com>
2022-04-04 17:12:55 +0400

    Async query
    Implemented query execute async.

--- ArcGIS_ServiceCallingWrapper/QueryTask.cs
+++ ArcGIS_ServiceCallingWrapper/QueryTask.cs
@@ -1,7 +1,6 @@
-﻿using Newtonsoft.Json;
-using System.Net;
+﻿using System;
 using System.Net.Http;
-using System;
+using System.Threading.Tasks;
 
 namespace ArcGIS_ServiceCallingWrapper.Helpers
 {
@@ -9,6 +8,10 @@ namespace ArcGIS_ServiceCallingWrapper.Helpers
     {
         private readonly string serviceUrl;
 
+        /// <summary>
+        /// Creates an instance of the QueryTask class usnig the given URL.
+        /// </summary>
+        /// <param name="url">Feature service URL</param>
         public QueryTask(string url)
         {
             this.serviceUrl = url;
@@ -26,8 +29,9 @@ namespace ArcGIS_ServiceCallingWrapper.Helpers
         /// <summary>
         /// Performs the query task with the given query object.
         /// </summary>
-        /// <param name="query"></param>
-        /// <returns></returns>
+        /// <param name="query">Query object.</param>
+        /// <returns>JSON response of the query task. Check remarks for details.</returns>
+        /// <remarks>For more details on the response, you may check "https://developers.arcgis.com/rest/services-reference/enterprise/query-map-service-layer-.htm#:~:text=object%20without%20geometry%0A%7D-,JSON%20Response%20examples,-The%20examples%20below"</remarks>
         public string Execute(Query query)
         {
             string jsonResult = null;
@@ -40,6 +44,29 @@ namespace ArcGIS_ServiceCallingWrapper.Helpers
                 var response = client.PostAsync(this.serviceUrl + "/query", query.Content).Result;
 
                 jsonResult = response.Content.ReadAsStringAsync().Result;
+            }
+
+            return jsonResult;
+        }
+
+        /// <summary>
+        /// Performs the query task with the given query object in asynchronous mode.
+        /// </summary>
+        /// <param name="query">Query object.</param>
+        /// <returns>Task that contains the JSON response of the query task. Check remarks for details.</returns>
+        /// <remarks>For more details on the response, you may check "https://developers.arcgis.com/rest/services-reference/enterprise/query-map-service-layer-.htm#:~:text=object%20without%20geometry%0A%7D-,JSON%20Response%20examples,-The%20examples%20below"</remarks>
+        public async Task<string> ExecuteAsync(Query query)
+        {
+            string jsonResult = null;
+
+            if (query.Content is null)
+                query.BuildQuery();
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = client.PostAsync(this.serviceUrl + "/query", query.Content).Result;
+
+                jsonResult = await response.Content.ReadAsStringAsync();
             }
 
             return jsonResult;
